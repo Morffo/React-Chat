e674a1e539b63fd5854e6c52b2d4546a5af9717e
Абдул Сулейманов <abdulsulejmanov@MacBook-Air-Abdul.local>
2025-03-22 00:43:30 +0300

    messages-verst

--- chat/src/components/Input.tsx
+++ chat/src/components/Input.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import attach from "../assets/attach.png"
+import img from "../assets/img.png"
 
 const Input = () => {
   return (
     <div className='input'>
-        Input
+        <input type="text" placeholder='Type something...' />
+        <div className="send">
+            <img src={attach} alt="" />
+            <input type="file" style={{display: "none"}} id="file"/>
+            <label htmlFor='file'>
+                <img src={img} alt="" />
+            </label>
+            <button>Send</button>
+        </div>
     </div>
   )
 }
--- chat/src/components/Message.tsx
+++ chat/src/components/Message.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>
-      djkwljed
+    <div className='message owner'>
+        <div className="messageInfo">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQksR3Lt2Iy2rlmUKvJmc27GcXpe297gINhTA&s" alt="" />
+            <span>just now</span>
+        </div>
+         <div className="messageContent">
+            <p>Hello</p>
+            {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQksR3Lt2Iy2rlmUKvJmc27GcXpe297gINhTA&s" alt="" /> */}
+         </div>
     </div>
   )
 }
